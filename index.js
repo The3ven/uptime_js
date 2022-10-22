da9067110c36ad040ac8ca76f6803c415396ff95
fire7ly <fire7ly@gmail.com>
2022-10-22 16:12:47 +0530

    Clenup
    Signed-off-by: fire7ly <fire7ly@gmail.com>

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { psup, sysup } = require ('./uptime')
 
 const i = 0;
 while (i < 10) {
-    let {ut_hour, ut_min, ut_sec} = psup();
+    let {ps_hour, ps_min, ps_sec} = psup();
     let {os_hour, os_min, os_sec} = sysup();
-    console.log(`process run time is : ${ut_hour}h ${ut_min}m ${ut_sec}s`);
+    console.log(`process run time is : ${ps_hour}h ${ps_min}m ${ps_sec}s`);
     console.log(`system run time is : ${os_hour}h ${os_min}m ${os_sec}s`);
 }
--- uptime.js
+++ uptime.js
@@ -1,29 +1,23 @@
 const os = require('os');
-
+const process = require('process');
 
 const psup = () => {
-    var ps_ut_sec = process.uptime();
-    var ps_ut_min = ps_ut_sec/60;
-    var ps_ut_hour = ps_ut_min/60;
-    ps_sec = Math.floor(ps_ut_sec);
-    ps_min = Math.floor(ps_ut_min);
-    ps_hour = Math.floor(ps_ut_hour);
-    ut_hour = ps_hour%60;
-    ut_min = ps_min%60;
-    ut_sec = ps_sec%60;
-    return { ut_sec, ut_min, ut_hour};
+    var ps_ut_sec = Math.floor(process.uptime());
+    var ps_ut_min = Math.floor(ps_ut_sec/60);
+    var ps_ut_hour = Math.floor(ps_ut_min/60);
+    ps_hour = ps_ut_hour%60;
+    ps_min = ps_ut_min%60;
+    ps_sec = ps_ut_sec%60;
+    return { ps_sec, ps_min, ps_hour};
 };
 
 const sysup = () => {
-    var os_ut_sec = os.uptime();
-    var os_ut_min = os_ut_sec/60;
-    var os_ut_hour = os_ut_min/60;
-    ut_sec = Math.floor(os_ut_sec)
-    ut_min = Math.floor(os_ut_min);
-    ut_hour = Math.floor(os_ut_hour);
-    os_hour = ut_hour%60;
-    os_min = ut_min%60;
-    os_sec = ut_sec%60;
+    var os_ut_sec = Math.floor(os.uptime());
+    var os_ut_min = Math.floor(os_ut_sec/60);
+    var os_ut_hour = Math.floor(os_ut_min/60);
+    os_hour = os_ut_hour%60;
+    os_min = os_ut_min%60;
+    os_sec = os_ut_sec%60;
     return { os_sec, os_min, os_hour};
 };
 
